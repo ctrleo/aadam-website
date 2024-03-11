7142ec24b6bbfb1cf4cb05c70c47a32c1ab2ec74
Leooo 3 <leokeene@icloud.com>
2024-03-11 12:47:47 +0000

    choice function

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-# empty file (for now)
+from pyscript import document
+def choice(out, one, two=None, three=None, four=None, five=None):
+    document.querySelector("#out").innerText = out
+    document.querySelector('#1').innerText = one
+    document.querySelector('#1').style.display = 'inline-block'
+    if two != None:
+        document.querySelector('#2').innerText = two
+        document.querySelector('#2').style.display = 'inline-block'
+    if three != None:
+        document.querySelector('#3').innerText = three
+        document.querySelector('#3').style.display = 'inline-block'
+    if four != None:
+        document.querySelector('#4').innerText = four
+        document.querySelector('#4').style.display = 'inline-block'
+    if five != None:
+        document.querySelector('#5').innerText = five
+        document.querySelector('#5').style.display = 'inline-block'
