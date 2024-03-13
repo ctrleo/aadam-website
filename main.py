44bfa45844199bf8b11a214fe9a81feb1c8e6015
Leooo 3 <120170440+ctrleo@users.noreply.github.com>
2024-03-13 09:12:36 +0000

    finished function

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from pyscript import document, when
+import time
+from pyscript import document
+button_bind = 0
+def write(text):
+    document.querySelector("#out").innerText = text
+def activate_button(button_number):
+    button_bind = button_number
 def choice(out, one, two=None, three=None, four=None, five=None):
     document.querySelector("#out").innerText = out
     document.querySelector('#one').innerText = one
@@ -15,5 +21,15 @@ def choice(out, one, two=None, three=None, four=None, five=None):
     if five != None:
         document.querySelector('#five').innerText = five
         document.querySelector('#five').style.display = 'inline-block'
+    while button_bind == 0:
+        waiting_for_input = True
+    return button_bind
 
-# pls add some code :3
+first = choice("New test choice", "A", "B", "C")
+match first:
+    case 1:
+        write("You chose A")
+    case 2:
+        write("You chose B")
+    case 3:
+        write("You chose C")
